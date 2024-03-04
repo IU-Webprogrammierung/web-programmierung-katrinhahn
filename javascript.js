b7c56ad4e714bfb11205e548003d7de996f70f80
katrinhahn <katrin.hahn1@iubh-fernstudium.de>
2024-03-04 13:02:00 +0100

    favicon hinzugefügt

--- javascript.js
+++ javascript.js
@@ -1,3 +1,9 @@
-function toggleMenu() {
-    document.getElementById("menu").classList.toggle("active");
-  }
+const hamburgerEvent = (navigation, open, close) => {
+  navigationItems.style.display = navigation;
+  openHam.style.display = open;
+  closeHam.style.display = close;
+};
+
+openHam.addEventListener('click', () => hamburgerEvent("flex", "none", "none"));
+
+document.querySelector('#closeHam').addEventListener('click', () => hamburgerEvent("none", "block", "block"));
