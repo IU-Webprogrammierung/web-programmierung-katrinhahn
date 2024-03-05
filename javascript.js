8363ddbffec5de8bf53b823f0be95493c83721c5
katrinhahn <katrin.hahn1@iubh-fernstudium.de>
2024-03-05 20:11:12 +0100

    add scroll to top button

--- javascript.js
+++ javascript.js
@@ -1,9 +1,15 @@
+document.addEventListener("scroll", (event) => {
+  if (window.scrollY > 300) {
+    document.getElementById('scroll-to-top-btn').style.display = 'block';
+  } else {
+    document.getElementById('scroll-to-top-btn').style.display = 'none';
+  }
+});
 const hamburgerEvent = (navigation, open, close) => {
-  navigationItems.style.display = navigation;
-  openHam.style.display = open;
-  closeHam.style.display = close;
+  document.getElementById('navigation-items').style.display = navigation;
+  document.getElementById('openHam').style.display = open;
+  document.getElementById('closeHam').style.display = close;
 };
-
-openHam.addEventListener('click', () => hamburgerEvent("flex", "none", "none"));
-
-document.querySelector('#closeHam').addEventListener('click', () => hamburgerEvent("none", "block", "block"));
+document.getElementById("openHam").addEventListener('click', () => hamburgerEvent("flex", "none", "block"));
+document.getElementById('closeHam').addEventListener('click', () => hamburgerEvent("none", "block", "none"));
+document.getElementById('scroll-to-top-btn').addEventListener('click', () => window.scrollTo({top: 0, behavior: "smooth"}));
